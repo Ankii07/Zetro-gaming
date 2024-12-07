4b9ed528e2dae8a57efe31d0db85580b53a35b2b
Ankii07 <108866932+Ankii07@users.noreply.github.com>
2024-12-07 17:28:13 +0530

    contact changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import About from "./components/About";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
