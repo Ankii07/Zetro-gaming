6343029a762da831abca9f779d7395301ecc2302
Ankii07 <108866932+Ankii07@users.noreply.github.com>
2024-12-07 17:23:25 +0530

    footer-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./components/About";
-// import Contact from "./components/contact";
+import Contact from "./components/contact";
 import Features from "./components/Features";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Story from "./components/Story";
@@ -14,7 +15,7 @@ export default function App() {
       <About/>
       <Features/>
       <Story/>
-      {/* <Contact/> */}
+      <Contact/>
       <Footer/>
 
     </main>
